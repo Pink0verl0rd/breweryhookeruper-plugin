fcf1c7fd22b406987c4016a6e6f20d5cdd90ed46
Jun Ivar Doydora <ivardoydora@gmail.com>
2023-08-24 14:14:02 -0400

    Init

---
+++ breweryhookeruper-plugin.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * 
+ * Plugin Name: BreweryHookerUper Plugin
+ * Description: This is a test plugin
+ * Version: 1.0.0
+ * Text Domain: breweryhookeruper-plugin
+ * 
+ */
+
+ // Check to make sure the method of acces is Abasulte from root
+ if( !defined('ABSPATH') ) {
+
+    die('You cannot be here');
+
+ }
+ if( !class_exists( 'ContactPlugin' ) ) {
+ class ContactPlugin {
+
+    public function __construct() {
+
+        define('BREWERYHOOKERUPER_PATH', plugin_dir_path( __FILE__ ));
+
+        define('BREWERYHOOKERUPER_URL', plugin_dir_url( __FILE__ ));
+
+        // require_once(BREWERYHOOKERUPER_PATH . '/vendor/autoload.php');
+
+    }
+
+    public function initialize(){
+
+        // include_once BREWERYHOOKERUPER_PATH . 'includes/utils.php';
+
+        // include_once BREWERYHOOKERUPER_PATH . 'includes/options-page.php';
+
+        // include_once BREWERYHOOKERUPER_PATH . 'includes/contact-form.php';
+
+    }
+
+ }
+
+ $contactPlugin = new ContactPlugin;
+
+ $contactPlugin->initialize();
+
+}
